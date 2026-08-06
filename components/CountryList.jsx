416bfa90e5afcd85c9828da8435eacb4b0616702
Nishant Garg <nishantgarg17282@gmail.com>
2026-08-06 11:14:02 +0530

    api testing

--- components/CountryList.jsx
+++ components/CountryList.jsx
@@ -34,10 +34,10 @@ export default function CountryList({query,region}){
         <div className="card-container">
             {
                 
-                CountriesData.length==0?<CountryListShimmer/>:CountriesData.filter((country)=>(country.name.common.toLowerCase().includes(query)||country.region.toLowerCase().includes(query))).map((country)=>{
+                CountriesData.length==0?<CountryListShimmer/>:CountriesData.filter((country)=>(country.names.common.toLowerCase().includes(query)||country.region.toLowerCase().includes(query))).map((country)=>{
                     // console.log(country);
                     
-                    return <CountryCard key={country.name.common} name={country.name.common} flag={country.flags.svg} population={country.population.toLocaleString('en-IN')} region={country.region} capital={country.capital?.[0]} data={country}/>
+                    return country.flag.url_svg && <CountryCard key={country.names.common} name={country.names.common} flag={country.flag.url_svg} population={country.population.toLocaleString('en-IN')} region={country.region} capital={country.capitals?.[0].name} data={country}/>
                     
                 })
                 
