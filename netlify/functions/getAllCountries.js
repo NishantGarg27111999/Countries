d44e1bc85f942d43e902a29659520b88f0642b4e
Nishant Garg <nishantgarg17282@gmail.com>
2026-08-06 09:54:12 +0530

    Corrected api url

--- netlify/functions/getAllCountries.js
+++ netlify/functions/getAllCountries.js
@@ -1,6 +1,6 @@
 exports.handler = async () => {
   const API_KEY = process.env.REST_COUNTRIES_API_KEY;
-  const res = await fetch('https://api.restcountries.com/v5/all', {
+  const res = await fetch('https://api.restcountries.com/countries/v5', {
     headers: { Authorization: `Bearer ${API_KEY}` }
   });
   const data = await res.json();
