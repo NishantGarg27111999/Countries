9d6c83941c100ad76b35de837166414a392e54d6
Nishant Garg <nishantgarg17282@gmail.com>
2026-08-10 17:39:50 +0530

    increased country list limit at api side

--- netlify/functions/getAllCountries.js
+++ netlify/functions/getAllCountries.js
@@ -1,6 +1,6 @@
 exports.handler = async () => {
   const API_KEY = process.env.REST_COUNTRIES_API_KEY;
-  const res = await fetch('https://api.restcountries.com/countries/v5', {
+  const res = await fetch('https://api.restcountries.com/countries/v5?limit=100', {
     headers: { Authorization: `Bearer ${API_KEY}` }
   });
   const data = await res.json();
